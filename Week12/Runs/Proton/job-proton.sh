39c0a8d71ffb675ba16ad328425ecf92e4b453da
mpanjali <mp6576@nyu.edu>
2024-04-12 15:22:05 -0400

    Final

---
+++ Week12/Runs/Proton/job-proton.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#SBATCH --job-name=AIMD
+
+#SBATCH --nodes=1
+
+#SBATCH --tasks-per-node=12
+
+#SBATCH --mem=80GB
+
+#SBATCH --time=24:00:00
+
+module load cp2k/openmpi/intel/20201212
+
+mpirun -np 12 cp2k.popt -i proton.inp -o proton_output.log
